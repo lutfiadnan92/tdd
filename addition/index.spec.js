082c9c07a7090a2253a8d16017afd1bee0cb8d3c
adnan <32415271+lutfiadnan92@users.noreply.github.com>
2023-07-05 16:09:48 +0700

    feature addition (#1)
    * chore: add 3 scripts
    
    * chore(jest): update the config
    
    * chore: create typescript config
    
    * chore: exclude build folder
    
    * feat: create addition function
    
    * test: create a unit test of the addition function

--- addition/index.spec.js
+++
@@ -1,9 +0,0 @@
-describe("addition function", () => {
-  test('return 2 when 1 + 1 === 2', () => {
-    const a = 1
-    const b = 1
-    const result = a + b
-    const output = 2
-    expect(result).toEqual(output)
-  })
-})
