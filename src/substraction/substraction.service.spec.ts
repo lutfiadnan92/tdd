a526532ef56c8c53c2ac051d8b2b80da11ec6d9d
adnan <32415271+lutfiadnan92@users.noreply.github.com>
2023-07-06 17:41:41 +0700

    merge branch master (#5)
    * feat: create substraction function
    
    * test: create unit test for substraction function

---
+++ src/substraction/substraction.service.spec.ts
@@ -0,0 +1,9 @@
+describe("substraction function", () => {
+  test("test with positive value", () => {
+    const a = 1
+    const b = 1
+    const result = a-b
+    const output = 0
+    expect(result).toEqual(output)
+  })
+})
