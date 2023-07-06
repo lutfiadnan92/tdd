d478c61e215cce7499ac97e3b7c08f4aa78177b8
adnan <32415271+lutfiadnan92@users.noreply.github.com>
2023-07-06 17:16:48 +0700

    Dev 003 (#4)
    * refactor: separate addition function
    
    * test: add some unit test schema for sum function
    
    test if the number is negative and decimal number

--- src/addition/addition.service.spec.ts
+++ src/addition/addition.service.spec.ts
@@ -1,11 +1,27 @@
 import { additionService } from "./addition.service"
 
 describe("addition function", () => {
-  test("return 2 when 1 + 1 === 2", () => {
+  test("test with positive number", () => {
     const a = 1
     const b = 1
     const result = additionService(a, b)
     const output = 2
+    expect(result).toEqual(output)
+  })
+
+  test("test with negative number", () => {
+    const a = -2
+    const b = 3
+    const result = additionService(a, b)
+    const output = 1
+    expect(result).toEqual(output)
+  })
+
+  test("test with decimal number", () => {
+    const a = 1.5
+    const b = 2.5
+    const result = additionService(a, b)
+    const output = 4
     expect(result).toEqual(output)
   })
 })
