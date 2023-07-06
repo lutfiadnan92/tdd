8fbea74ec7e58562d7f1d0e5dc764d8440a68f03
lutfiadnan92 <adnanlutfi92@gmail.com>
2023-07-06 15:35:17 +0700

    feat: create substraction function

---
+++ src/substraction/substraction.service.spec.ts
@@ -0,0 +1,11 @@
+// import { additionService } from "./substraction.service"
+
+describe("substraction function", () => {
+  test("test with positive value", () => {
+    const a = 1
+    const b = 1
+    const result = a-b
+    const output = 0
+    expect(result).toEqual(output)
+  })
+})
